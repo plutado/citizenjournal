6bd738f18fc14f48c2874817b69744213bc6022e
plutado <david@davidfugate.com>
2013-02-23 19:20:18 -0500

    replacing reset with html5boilerplate's normalize.css

--- sites/all/themes/citizenjournal/templates/html.tpl.php
+++ sites/all/themes/citizenjournal/templates/html.tpl.php
@@ -11,18 +11,35 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0,maximum-scale=1.0">
   <?php print $styles; ?>
-  <?php print $scripts; ?>
 </head>
 
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
-  <header class="main">
-    <h1 class="logo"><a href="/" class="ss-list">DPF</a></h1> 
+
+  <!--[if lt IE 7]>
+    <p class="chromeframe">
+      You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> 
+      or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.
+    </p>
+  <![endif]-->
+  
+  <header class="main">    
+    <!-- if site logo -->
+    <?php if ($logo) : ?>
+      <a href="/" class="ss-list">
+        <img src="<?php print $logo; ?>" ></img>
+      </a>   
+    <?php endif; ?> 
+    <!-- /END site logo -->
+  </header>  
 
     <?php print $page; ?>
 
   <footer class="main">
+    
   </footer>
 
+  <?php print $scripts; ?>
+  
   <script>
     // Basic FitVids Test
     $(".content").fitVids({ customSelector: "iframe[src^='http://vimeo.com']"});  
