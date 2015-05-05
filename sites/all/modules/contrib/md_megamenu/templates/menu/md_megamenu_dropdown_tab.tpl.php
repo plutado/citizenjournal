04109b0f477dbeebb2408cd3a0d5dd7577aa3af3
David Plutado Fugate <david@davidfugate.com>
2015-05-04 23:24:38 -0400

    Clean up repo to start fresh

--- sites/all/modules/contrib/md_megamenu/templates/menu/md_megamenu_dropdown_tab.tpl.php
+++
@@ -1,4 +0,0 @@
-<li class="<?php print $tab_classes?>">
-<?php print $tab->content;?>
-<?php print $tab_content; ?>
-</li>
--- sites/all/themes/dpf/templates/field/field--field-feature-photo--blog-entry.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php foreach ($items as $delta => $item) : ?>
-  <div class="swiper-slide">
-    <div class="inner">
-      <?php print render($item); ?>
-    </div>
-  </div>
-<?php endforeach; ?>
