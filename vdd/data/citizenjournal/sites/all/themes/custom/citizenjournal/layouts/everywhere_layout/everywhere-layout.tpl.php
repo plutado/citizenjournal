fd6c220e775c6773272a5ac85bcf0d771311691b
David Plutado Fugate <david@davidfugate.com>
2015-08-01 03:31:37 -0400

    menu tweaks

--- vdd/data/citizenjournal/sites/all/themes/custom/citizenjournal/layouts/everywhere_layout/everywhere-layout.tpl.php
+++ vdd/data/citizenjournal/sites/all/themes/custom/citizenjournal/layouts/everywhere_layout/everywhere-layout.tpl.php
@@ -51,11 +51,8 @@
 <div class="overlay overlay-hugeinc">
   <nav>
     <ul>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Work</a></li>
-      <li><a href="#">Clients</a></li>
-      <li><a href="#">Contact</a></li>
+      <li><a href="/">Home</a></li>
+      <li><a href="/resume">Resume</a></li>
     </ul>
   </nav>
 </div>
